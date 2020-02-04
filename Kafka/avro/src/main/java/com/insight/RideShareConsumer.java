126e3928addc482b8946a2b64e9f62b253b58e9b
sam <sebastian.andreas.merkt@cern.ch>
2020-02-04 09:22:05 -0800

    Introduced RideShareStreamer.java

--- Kafka/avro/src/main/java/com/insight/RideShareConsumer.java
+++ Kafka/avro/src/main/java/com/insight/RideShareConsumer.java
@@ -71,30 +71,37 @@ public class RideShareConsumer {
                     final String vendor_name = record.value().get("vendor_name").toString();
                     final String Trip_Pickup_DateTime = record.value().get("Trip_Pickup_DateTime").toString();
                     final String Trip_Dropoff_DateTime = record.value().get("Trip_Dropoff_DateTime").toString();
-                    final long Passenger_Count = (long) record.value().get("Passenger_Count");
+//                    final long Passenger_Count = (long) record.value().get("Passenger_Count");
                     final double Trip_Distance = (double) record.value().get("Trip_Distance");
                     final double Start_Lon = (double) record.value().get("Start_Lon");
                     final double Start_Lat = (double) record.value().get("Start_Lat");
-                    final double Rate_Code = (double) record.value().get("Rate_Code");
-                    final double store_and_forward = (double) record.value().get("store_and_forward");
+//                    final double Rate_Code = (double) record.value().get("Rate_Code");
+//                    final double store_and_forward = (double) record.value().get("store_and_forward");
                     final double End_Lon = (double) record.value().get("End_Lon");
                     final double End_Lat = (double) record.value().get("End_Lat");
-                    final String Payment_Type = record.value().get("Payment_Type").toString();
-                    final double Fare_Amt = (double) record.value().get("Fare_Amt");
-                    final double surcharge = (double) record.value().get("surcharge");
-                    final double mta_tax = (double) record.value().get("mta_tax");
-                    final double Tip_Amt = (double) record.value().get("Tip_Amt");
-                    final double Tolls_Amt = (double) record.value().get("Tolls_Amt");
+//                    final String Payment_Type = record.value().get("Payment_Type").toString();
+//                    final double Fare_Amt = (double) record.value().get("Fare_Amt");
+//                    final double surcharge = (double) record.value().get("surcharge");
+//                    final double mta_tax = (double) record.value().get("mta_tax");
+//                    final double Tip_Amt = (double) record.value().get("Tip_Amt");
+//                    final double Tolls_Amt = (double) record.value().get("Tolls_Amt");
                     final double Total_Amt = (double) record.value().get("Total_Amt");
 
                     Statement stmt = dbConn.createStatement();
 
+
+//                    String sql = "INSERT INTO yellowcabsV1 " +
+//                            "VALUES ('" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+"', "+
+//                            Passenger_Count+", "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+Rate_Code+", "+
+//                            store_and_forward+", "+End_Lon+", "+End_Lat+", '"+Payment_Type+"', "+
+//                            Fare_Amt+", "+surcharge+", "+mta_tax+", "+Tip_Amt+", "+Tolls_Amt+", "+Total_Amt+
+//                            ", 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")' "+")";
+
+
                     String sql = "INSERT INTO yellowcabsV1 " +
-                            "VALUES ('" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+"', "+
-                            Passenger_Count+", "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+Rate_Code+", "+
-                            store_and_forward+", "+End_Lon+", "+End_Lat+", '"+Payment_Type+"', "+
-                            Fare_Amt+", "+surcharge+", "+mta_tax+", "+Tip_Amt+", "+Tolls_Amt+", "+Total_Amt+
-                            ", 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")' "+")";
+                            "VALUES ('" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+
+                            ", "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+End_Lon+", "+End_Lat+"', "+
+                            ", "+Total_Amt+", 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")' "+")";
 
                     stmt.executeUpdate(sql);
                 }
