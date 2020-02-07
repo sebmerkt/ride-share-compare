881c7c91bb76a0b0a36a04f5bda50116111ab34c
sam <sebastian.andreas.merkt@cern.ch>
2020-02-07 12:09:58 -0800

    Added schema V5

---
+++ Kafka/RideShareProducer/src/main/java/com/insight/RideShareProducerV5.java
@@ -0,0 +1,47 @@
+package com.insight;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class RideShareProducerV5 extends RideShareProducerBase <Ride5> {
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void main(final String[] args) throws IOException {
+
+        final Properties props = initProperties();
+
+        RideShareProducerV5 rideShareProducer = new RideShareProducerV5( );
+
+        rideShareProducer.setRide( new Ride5() );
+
+        rideShareProducer.setProducer( new KafkaProducer<>(props));
+
+        rideShareProducer.sendRecords(args);
+        rideShareProducer.getProducer().flush();
+        rideShareProducer.getProducer().close();
+    }
+
+
+    @Override
+    void buildRecord( final String[] message) {
+        ride.setVendorName( InsertString(message[0]) );
+        ride.setTripPickupDateTime( InsertString(message[1]) );
+        ride.setTripDropoffDateTime( InsertString(message[2]) );
+        ride.setPassengerCount( InsertInt(message[3]) );
+        ride.setTripDistance( InsertDouble(message[4]) );
+        ride.setPULocationID( InsertLong(message[5]) );
+        ride.setRateCode( InsertDouble(message[6]) );
+        ride.setStoreAndForward( InsertDouble(message[7]) );
+        ride.setDOLocationID( InsertLong(message[8]) );
+        ride.setPaymentType( InsertString(message[9]) );
+        ride.setFareAmt( InsertDouble(message[10]) );
+        ride.setExtra( InsertDouble(message[11]) );
+        ride.setSurcharge( InsertDouble(message[12]) );
+        ride.setMtaTax( InsertDouble(message[13]) );
+        ride.setTipAmt( InsertDouble(message[14]) );
+        ride.setTollsAmt( InsertDouble(message[15]) );
+        ride.setTotalAmt( InsertDouble(message[16]) );
+    }
+}
