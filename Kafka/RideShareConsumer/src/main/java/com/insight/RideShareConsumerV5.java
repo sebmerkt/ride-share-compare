da333a72fa77514c23292e1ea3ee10f02bfd52ba
sam <sebastian.andreas.merkt@cern.ch>
2020-02-07 18:26:41 -0800

    Added schema V5

--- Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV5.java
+++ Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV5.java
@@ -37,9 +37,13 @@ public class RideShareConsumerV5 extends RideShareConsumerBase {
                     final String Trip_Dropoff_DateTime = InsertString(record.value().get("Trip_Dropoff_DateTime"));
                     final int Passenger_Count = InsertInt(record.value().get("Passenger_Count"));
                     final double Trip_Distance = InsertDouble(record.value().get("Trip_Distance"));
+                    final double Start_Lon = InsertDouble(record.value().get("Start_Lon"));
+                    final double Start_Lat = InsertDouble(record.value().get("Start_Lat"));
                     final long PULocationID = InsertLong(record.value().get("PULocationID"));
                     final double Rate_Code = InsertDouble(record.value().get("Rate_Code"));
                     final double store_and_forward = InsertDouble(record.value().get("store_and_forward"));
+                    final double End_Lon = InsertDouble(record.value().get("End_Lon"));
+                    final double End_Lat = InsertDouble(record.value().get("End_Lat"));
                     final long DOLocationID = InsertLong(record.value().get("DOLocationID"));
                     final String Payment_Type = record.value().get("Payment_Type").toString();
                     final double Fare_Amt = InsertDouble(record.value().get("Fare_Amt"));
@@ -50,11 +54,11 @@ public class RideShareConsumerV5 extends RideShareConsumerBase {
                     final double Total_Amt = InsertDouble(record.value().get("Total_Amt"));
 
                     Statement stmt = dbConn.createStatement();
-
+                    
                     String sql = "INSERT INTO ride_share_A_v4 " +
                             "VALUES ('" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+"', "+
-                            Passenger_Count+", "+Trip_Distance+", "+PULocationID+", "+Rate_Code+", "+
-                            store_and_forward+", "+DOLocationID+", '"+Payment_Type+"', "+
+                            Passenger_Count+", "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+Rate_Code+", "+
+                            store_and_forward+", "+End_Lon+", "+End_Lat+", '"+Payment_Type+"', "+
                             Fare_Amt+", "+surcharge+", "+mta_tax+", "+Tip_Amt+", "+Tolls_Amt+", "+Total_Amt+
                             ", 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")' "+")";
 
