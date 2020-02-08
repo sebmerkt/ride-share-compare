84a959226cc8da51b107734df7970d6ab8fb4658
sam <sebastian.andreas.merkt@cern.ch>
2020-02-07 17:12:08 -0800

    Added schema V5

--- Kafka/RideShareProducer/src/main/java/com/insight/RideShareProducerV5.java
+++ Kafka/RideShareProducer/src/main/java/com/insight/RideShareProducerV5.java
@@ -31,21 +31,21 @@ public class RideShareProducerV5 extends RideShareProducerBase <Ride5> {
         ride.setTripDropoffDateTime( InsertString(message[2]) );
         ride.setPassengerCount( InsertInt(message[3]) );
         ride.setTripDistance( InsertDouble(message[4]) );
-        ride.setStartLon( InsertDouble(message[5]) );
-        ride.setStartLat( InsertDouble(message[6]) );
-        ride.setPULocationID( InsertLong(message[7]) );
-        ride.setRateCode( InsertDouble(message[8]) );
-        ride.setEndLon( InsertDouble(message[9]) );
-        ride.setEndLat( InsertDouble(message[10]) );
-        ride.setStoreAndForward( InsertDouble(message[11]) );
-        ride.setDOLocationID( InsertLong(message[12]) );
-        ride.setPaymentType( InsertString(message[13]) );
-        ride.setFareAmt( InsertDouble(message[14]) );
-        ride.setExtra( InsertDouble(message[15]) );
-        ride.setSurcharge( InsertDouble(message[16]) );
-        ride.setMtaTax( InsertDouble(message[17]) );
-        ride.setTipAmt( InsertDouble(message[18]) );
-        ride.setTollsAmt( InsertDouble(message[19]) );
-        ride.setTotalAmt( InsertDouble(message[20]) );
+//        ride.setStartLon( InsertDouble(message[5]) );
+//        ride.setStartLat( InsertDouble(message[6]) );
+        ride.setPULocationID( InsertLong(message[5]) );
+        ride.setRateCode( InsertDouble(message[6]) );
+//        ride.setEndLon( InsertDouble(message[9]) );
+//        ride.setEndLat( InsertDouble(message[10]) );
+        ride.setStoreAndForward( InsertDouble(message[7]) );
+        ride.setDOLocationID( InsertLong(message[7]) );
+        ride.setPaymentType( InsertString(message[8]) );
+        ride.setFareAmt( InsertDouble(message[9]) );
+        ride.setExtra( InsertDouble(message[10]) );
+        ride.setSurcharge( InsertDouble(message[11]) );
+        ride.setMtaTax( InsertDouble(message[12]) );
+        ride.setTipAmt( InsertDouble(message[13]) );
+        ride.setTollsAmt( InsertDouble(message[14]) );
+        ride.setTotalAmt( InsertDouble(message[25]) );
     }
 }
