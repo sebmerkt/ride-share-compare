349131fb630cd4d1277c416c5380876fcfb39b4d
sam <sebastian.andreas.merkt@cern.ch>
2020-02-13 13:36:33 -0800

    Updated rolling deployment

--- Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV5.java
+++ Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV5.java
@@ -86,7 +86,7 @@ public class RideShareConsumerV5 extends RideShareConsumerBase {
                             "Trip_Distance, Start_Lon, Start_Lat, Rate_Code, store_and_forward, End_Lon, End_Lat, " +
                             "Payment_Type, Fare_Amt, surcharge, mta_tax, Tip_Amt, Tolls_Amt, Total_Amt, " +
                             "Process_time, geom_start, geom_end, ) " +
-                            "VALUES ( "+uuid+", '" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+"', "+
+                            "VALUES ( '"+uuid+"', '" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+Trip_Dropoff_DateTime+"', "+
                             Passenger_Count+", "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+Rate_Code+", "+
                             store_and_forward+", "+End_Lon+", "+End_Lat+", '"+Payment_Type+"', "+
                             Fare_Amt+", "+surcharge+", "+mta_tax+", "+Tip_Amt+", "+Tolls_Amt+", "+Total_Amt+", '"+Process_time+
