145f473a8a8b6cb780a1d42f4ffdf1bfbfb3db2c
sam <sebastian.andreas.merkt@cern.ch>
2020-02-13 13:15:34 -0800

    Updated rolling deployment

--- rolling_deployment/run_rolling_deployment.sh
+++ rolling_deployment/run_rolling_deployment.sh
@@ -5,29 +5,29 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Start code version 1
 # Start streamer
 
-# cd $SCRIPT_DIR"/../Kafka/RideShareStreamer/target/"
+cd $SCRIPT_DIR"/../Kafka/RideShareStreamer/target/"
 
-# nohup java -jar RideShareStreamerV1.jar > ~/log/RideShareStreamerV1.log 2>&1 & stream_v1_process_id=$!
-# echo "Sreamer V1 running with PID "$stream_v1_process_id
+nohup java -jar RideShareStreamerV1.jar > ~/log/RideShareStreamerV1.log 2>&1 & stream_v1_process_id=$!
+echo "Sreamer V1 running with PID "$stream_v1_process_id
 
-# # Start consumer
+# Start consumer
 
-# cd $SCRIPT_DIR"/../Kafka/RideShareConsumer/target/"
+cd $SCRIPT_DIR"/../Kafka/RideShareConsumer/target/"
 
-# nohup java -jar RideShareConsumerV1.jar > ~/log/RideShareConsumerV1.log 2>&1 & cons_v1_process_id=$!
-# echo "Consumer V1 running with PID "$cons_v1_process_id
+nohup java -jar RideShareConsumerV1.jar > ~/log/RideShareConsumerV1.log 2>&1 & cons_v1_process_id=$!
+echo "Consumer V1 running with PID "$cons_v1_process_id
 
-# # Start producer
+# Start producer
 
-# cd $SCRIPT_DIR"/../Kafka/RideShareProducer/target/"
+cd $SCRIPT_DIR"/../Kafka/RideShareProducer/target/"
 
-# nohup java -jar RideShareProducerV1.jar ~/nyc-taxi-rideshare/schema_evolution_data/yellow_tripdata_2009-01_V1_full.csv > ~/log/RideShareProducerV1.log 2>&1 &  prod_v1_process_id=$!
-# echo "Producer V1 running with PID "$prod_v1_process_id
-
-
+nohup java -jar RideShareProducerV1.jar ~/nyc-taxi-rideshare/schema_evolution_data/yellow_tripdata_2009-01_V1_full.csv > ~/log/RideShareProducerV1.log 2>&1 &  prod_v1_process_id=$!
+echo "Producer V1 running with PID "$prod_v1_process_id
 
 
-# # Let it run for a while
+
+
+# Let it run for a while
 
 # echo "Waiting for 1 minutes"
 # sleep 1m
@@ -38,11 +38,11 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Add new schema version 2
 # Evolve database to accomodate schema version 2: Ride2
 
-echo "Evolving database"
-python3 $SCRIPT_DIR/update_database.py "Passenger_Count" "int8" "Fare_Amt" "float8" "Tip_Amt" "float8" & db_process_id=$!
+# echo "Evolving database"
+# python3 $SCRIPT_DIR/update_database.py "Passenger_Count" "int8" "Fare_Amt" "float8" "Tip_Amt" "float8" & db_process_id=$!
 
-wait $db_process_id
-echo "Database schema evolved to V2 with status "$?
+# wait $db_process_id
+# echo "Database schema evolved to V2 with status "$?
 
 # Start new producer/streamer/consumer
 
