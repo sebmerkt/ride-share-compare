ad3768ae3461df6706acb345054a82a2bb2a3da4
sam <sebastian.andreas.merkt@cern.ch>
2020-02-12 18:09:32 -0800

    Updated rolling deployment

--- rolling_deployment/run_rolling_deployment.sh
+++ rolling_deployment/run_rolling_deployment.sh
@@ -58,7 +58,7 @@ echo "Consumer V2 running with PID "$cons_v2_process_id
 
 cd $SCRIPT_DIR"/../Kafka/RideShareProducer/target/"
 
-nohup java -jar RideShareProducerV2.jar ~/nyc-taxi-rideshare/schema_evolution_data/yellow_tripdata_2009-02_V2_full.csv > ~/log/RideShareProducerV2.log 2>&1 & & prod_v2_process_id=$!
+nohup java -jar RideShareProducerV2.jar ~/nyc-taxi-rideshare/schema_evolution_data/yellow_tripdata_2009-02_V2_full.csv > ~/log/RideShareProducerV2.log 2>&1 & prod_v2_process_id=$!
 echo "Producer V2 running with PID "$prod_v2_process_id
 
 
