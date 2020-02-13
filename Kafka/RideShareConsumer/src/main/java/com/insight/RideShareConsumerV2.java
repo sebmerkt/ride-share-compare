a84509d11ad6a39431d564e1d7ad4a3af5992384
sam <sebastian.andreas.merkt@cern.ch>
2020-02-13 14:15:46 -0800

    Updated rolling deployment

--- Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV2.java
+++ Kafka/RideShareConsumer/src/main/java/com/insight/RideShareConsumerV2.java
@@ -78,7 +78,7 @@ public class RideShareConsumerV2 extends RideShareConsumerBase {
                             "VALUES ( "+uuid+", '" + vendor_name+"', '"+Trip_Pickup_DateTime+"', '"+
                                 Trip_Dropoff_DateTime+"', "+Trip_Distance+", "+Start_Lon+", "+Start_Lat+", "+
                                 +End_Lon+", "+End_Lat+", "+Total_Amt+", '"+Process_time+
-                                "', 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")' "+
+                                "', 'SRID=4326;POINT("+Start_Lon+" "+Start_Lat+")', 'SRID=4326;POINT("+End_Lon+" "+End_Lat+")', "+
                                 Passenger_Count+", "+Fare_Amt+", "+Tip_Amt+")";
                     System.out.println(sql);
                     stmt.executeUpdate(sql);
