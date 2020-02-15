3c73e7ad8c9937138dfb426fd5a77b225bc95fc9
sam <sebastian.andreas.merkt@cern.ch>
2020-02-15 15:04:05 -0800

    Added click points on map

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ import os
 from datetime import datetime, timedelta
 import geocoder
 import time
+from numpy import round
 
 import plotly.graph_objects as go
 
@@ -76,7 +77,7 @@ def display_click_data(clickData):
   ret = ""
   if clickData:
     if clickData["points"][0]["customdata"][1] >0:
-      fare_per_dist = clickData["points"][0]["customdata"][0]/clickData["points"][0]["customdata"][1]
+      fare_per_dist = numpy.round( clickData["points"][0]["customdata"][0]/clickData["points"][0]["customdata"][1], decimals=2 )
     else:
       fare_per_dist = "not available"
     
