3dceb0605f0dd9edf6e1cd4b3e39effb1408285d
sam <sebastian.andreas.merkt@cern.ch>
2020-02-14 16:26:21 -0800

    Update app

--- app/app.py
+++ app/app.py
@@ -75,7 +75,7 @@ def make_figure(n,coord):
 
     lendf=0
     multi=1
-    while( lendf<4):
+    while( lendf==0):
       now = datetime.utcnow()
       five_minutes_ago = now - timedelta(hours=0, minutes=0, seconds=10)
 
