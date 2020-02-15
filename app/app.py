5d3a27f04eae911d09b97e4efbf007b4c905aef2
sam <sebastian.andreas.merkt@cern.ch>
2020-02-15 15:36:26 -0800

    Added click points on map

--- app/app.py
+++ app/app.py
@@ -168,7 +168,7 @@ def make_figure(n,input_value):
                   color='Magenta',
                   size=10
               ),
-      hovertemplate = ['Lyft' for i in range(len(lons_lyft))]
+      hovertemplate = ['Lyft' for i in range(len(lons_lyft))],
       customdata=df[["total_amt","trip_distance"]],
       text=["Lyft"],
       ), 
