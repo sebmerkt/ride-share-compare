3fb3e49736b9d1211135fbc2f1d7582ed4ac13d9
sam <sebastian.andreas.merkt@cern.ch>
2020-02-19 18:26:27 -0800

    Change Dash style

--- app/app.py
+++ app/app.py
@@ -147,7 +147,7 @@ def make_figure(n,input_value):
 
       create_table_query = '''SELECT vendor_name, total_amt, trip_distance, end_lon, end_lat, ST_Distance(ST_Transform(geom_end::geometry, 3857), ST_Transform('SRID=4326;POINT( %s %s )'::geometry, 3857))
         AS distance
-        FROM ride_share_data WHERE ST_DWithin(geom_end, ST_GeographyFromText('SRID=4326;POINT( %s %s  )'), %s) '''%(lon, lat, lon, lat, radius)
+        FROM ride_share_data WHERE ST_DWithin(geom_end, ST_GeographyFromText('SRID=4326;POINT( %s %s  )'), %s)  FETCH FIRST 15 ROWS ONLY '''%(lon, lat, lon, lat, radius)
 
       # fetch data from PostGIS and save in pandas dataframe
       df = pd.read_sql_query(create_table_query, connection)
