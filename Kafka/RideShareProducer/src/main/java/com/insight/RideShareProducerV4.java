92f900d1e223bdca8c67f6f89a7b9a14cd2cbb7c
sam <sebastian.andreas.merkt@cern.ch>
2020-02-21 11:12:40 -0800

    Change Dash style

--- Kafka/RideShareProducer/src/main/java/com/insight/RideShareProducerV4.java
+++ Kafka/RideShareProducer/src/main/java/com/insight/RideShareProducerV4.java
@@ -67,7 +67,7 @@ public class RideShareProducerV4 extends RideShareProducerBase <Ride4> {
         ride.setEndLat( InsertDouble(message[10]) );
         ride.setPaymentType( InsertString(message[11]) );
         ride.setFareAmt( InsertDouble(message[12]) );
-        ride.setExtra( InsertDouble(message[13) );
+        ride.setExtra( InsertDouble(message[13]) );
         ride.setMtaTax( InsertDouble(message[14]) );
         ride.setTipAmt( InsertDouble(message[15]) );
         ride.setTollsAmt( InsertDouble(message[16]) );
