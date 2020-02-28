696e7d455a97f7f50797867216af0307ea272ece
sam <sebastian.andreas.merkt@cern.ch>
2020-02-27 16:22:11 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -226,7 +226,7 @@ def make_figure(n,input_value):
       go.Choroplethmapbox(geojson=city_locations, colorscale=color_range,
                           z=rides_per_loc,
                           locations=df_loc.dolocationid, featureidkey="properties.LocationID",
-                          hovertemplate = ['%s rides in neighborhoods'%i for i in rides_per_loc],
+                          hovertemplate = get_num_rides(df_loc)#['%s rides in neighborhoods'%i for i in rides_per_loc],
                           # custom_data=get_num_rides(df_loc),
                           text=["Rides"],
                           # showscale=True,
