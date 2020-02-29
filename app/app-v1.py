f76964d312342184ec697723ed7a2756e69ef684
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 09:17:29 -0800

    Introduced new app version

--- app/app-v1.py
+++ app/app-v1.py
@@ -36,7 +36,7 @@ except (Exception, psycopg2.DatabaseError) as error:
 
 # Initialize app
 app = dash.Dash(
-    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+    __name__, external_stylesheets=["/assets/bootstrap.css"]
 )
 
 # Define the layout
--- app/app-v2.py
+++ app/app-v2.py
@@ -40,7 +40,7 @@ except (Exception, psycopg2.DatabaseError) as error:
 
 # Initialize app
 app = dash.Dash( 
-    __name__, external_stylesheets=["/assets/darkly.bootstrap.css"]
+    __name__, external_stylesheets=["/assets/bootstrap.css"]
 )
 
 # Define the layout
