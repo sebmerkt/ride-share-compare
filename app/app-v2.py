40250d717d39cc2686547b533429302333f9758f
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 11:31:46 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -88,7 +88,7 @@ app.layout = html.Div(
       # Show the map
       html.Div([
         dcc.Graph(id="graph", style={"width": "100%", "display": "inline-block"}),
-      ], style={'width': '75%', 'display': 'inline-block', 'color': colors['plotly_blue']}),
+      ], style={'width': '75%', 'display': 'inline-block', 'color': colors['plotly_blue'], "background": "#333432"}),
     ],
     style={ "background": "#333432", 'color': colors['plotly_blue'],},
     ),
