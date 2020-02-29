b9d02650ae46f677882ff48b7bf1284f6b0d313d
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 11:10:51 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -224,6 +224,7 @@ def make_figure(n_interval, n_clicks, input_value):
     else:  
       color_range=['rgba(%s,%s,%s,0.3)'%(int(i/max(rides_per_loc)*200), int(i/max(rides_per_loc)*255), int(100+i/max(rides_per_loc)*155)) for i in list(reversed(sorted(rides_per_loc.unique())))]
     
+    print(color_range)
     if lendf>0:
     # Define the data
       data = [
