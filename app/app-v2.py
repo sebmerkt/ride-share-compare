f22684612ff5e462907b6ce1810306516e01e501
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 09:28:58 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -3,7 +3,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 import pandas as pd
 from numpy import round,arange
