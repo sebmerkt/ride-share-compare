6078c2418d3978e34d80e3f775b8b146537ebb9e
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 11:44:06 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -332,9 +332,8 @@ def make_figure(n_interval, n_clicks, input_value):
       paper_bgcolor='rgba(0,0,0,0)',
       plot_bgcolor='rgba(0,0,0,0)',
       font=dict(
-        family="Courier New, monospace",
         size=18,
-        color="#7f7f7f"
+        color=color['plotly_blue']
     )
     )
 
