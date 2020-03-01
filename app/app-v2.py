cf2f50fe171ee4f3bb8fd0ff505532488610a9fb
sam <sebastian.andreas.merkt@cern.ch>
2020-02-29 21:57:18 -0800

    Introduced new app version

--- app/app-v2.py
+++ app/app-v2.py
@@ -255,7 +255,7 @@ def make_figure(n_interval, n_clicks, input_value):
         lon=lons_citibike,
         mode='markers', name='Citi Bike', 
         # marker={'color': colors['CitiBike'], 'size': 10, 'symbol': "bicycle"},
-        marker_symbol='bicycle'
+        marker_symbol='bicycle',
         marker_color=colors['CitiBike'],
         # marker_line_color="lightskyblue",
         hovertemplate = ['Citi Bike' for i in range(len(lons_citibike))],
