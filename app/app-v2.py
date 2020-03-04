f7fa200a30bd12e70ba27df607bf5d6f431c4918
sam <sebastian.andreas.merkt@cern.ch>
2020-03-03 17:06:35 -0800

    Change app to light mode

--- app/app-v2.py
+++ app/app-v2.py
@@ -81,7 +81,7 @@ app.layout = html.Div(
       html.B("Enter your pickup location:  "),
         dcc.Input(id='my-id', value='11 Wall Street, New York', type='text', style={ 'textAlign': 'left',
                     'color': colors['plotly_blue'], "background": colors['background']}),
-        html.Btn('Update', id='button', style={'color': colors['plotly_blue']}),
+        html.Button('Update', id='button', className='btn', style={'color': colors['plotly_blue']}),
         ],
         style={ 'textAlign': 'left',
                     'color': colors['plotly_blue'],
