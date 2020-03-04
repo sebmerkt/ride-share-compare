ae4de6dfd2ac73f9beee629790449a3ad94dd008
sam <sebastian.andreas.merkt@cern.ch>
2020-03-03 17:11:40 -0800

    Change app to light mode

--- app/app-v2.py
+++ app/app-v2.py
@@ -79,7 +79,7 @@ app.layout = html.Div(
     # Input field for address search
     html.P([
       html.B("Enter your pickup location:  "),
-        dcc.Input(id='my-id', value='11 Wall Street, New York', type='text', className='form-inline my-2 mr-sm-2' style={ 'textAlign': 'left',
+        dcc.Input(id='my-id', value='11 Wall Street, New York', type='text', className='form-inline my-2 mr-sm-2', style={ 'textAlign': 'left',
                     'color': colors['plotly_blue']}),
         html.Button('Update', id='button', className='btn btn-primary'),
         ],
