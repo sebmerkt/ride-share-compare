b195b8820d4bb82bbbfc6779771b4d5d261cc77f
sam <sebastian.andreas.merkt@cern.ch>
2020-03-04 10:53:22 -0800

    Change app to light mode

--- app/app-v2.py
+++ app/app-v2.py
@@ -339,7 +339,7 @@ def make_figure(n_interval, n_clicks, input_value):
   layout = go.Layout(
     autosize=True,
     # width=1000,
-    height=900, 
+    height=700, 
     # Center around user position
     mapbox=dict( accesstoken=token,
                   center=dict( lat=lat, lon=lon ),
